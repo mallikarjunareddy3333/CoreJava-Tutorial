6c6883ed00aa212ccf5a4ce998e4b9d9bf7c5d13
Arjun <mallikarjunareddy3333@gmail.com>
2023-08-19 23:41:10 +0530

    Core Java - Variables and Data Types

---
+++ src/demo/ValiablesDemo.java
@@ -0,0 +1,27 @@
+package demo;
+
+public class ValiablesDemo {
+
+	public static void main(String[] args) 
+	{
+		// <datatype> valiable_name = <value>;
+		
+		int num1 = 200;
+		int num2 = 300;
+		
+		float rate = 0.2f;
+		double rate1 = 0.34;
+		
+		char initial = 'A';
+		
+		String name = "Java";
+		
+		boolean male = true;
+		
+		int sum = num1 + num2;
+		
+		System.out.print(num1 + " + " + num2 + " = " + sum);
+		
+	}
+
+}

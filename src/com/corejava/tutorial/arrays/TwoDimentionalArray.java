ae036b434853b5871dfd7f5fa4b211886b8f4eda
Arjun <mallikarjunareddy3333@gmail.com>
2023-09-02 10:41:53 +0530

    - Program to print 2-D Array values

--- src/com/corejava/tutorial/arrays/TwoDimentionalArray.java
+++ src/com/corejava/tutorial/arrays/TwoDimentionalArray.java
@@ -4,7 +4,8 @@ public class TwoDimentionalArray {
 
 	public static void main(String[] args) 
 	{
-		char[][] ticTacToeArray = new char[3][3]; // declaration
+		char[][] ticTacToeArray = new char[4][3]; // declaration
+		
 		//  	0 1 2
 		//
 		//  0 	O X X
@@ -26,13 +27,99 @@ public class TwoDimentionalArray {
 		ticTacToeArray[2][1] = 'O';
 		ticTacToeArray[2][2] = 'O';
 		
-		System.out.println(ticTacToeArray[2][2]);
+		//System.out.println(ticTacToeArray[2][2]);
 		
-		char[][] ticTacToeArray2 = {{'O', 'X', 'X'}, {'O', 'X', 'X'}, {'O', 'X', 'X'}}; // declaration
-		
-		System.out.println(ticTacToeArray2[2][2]);
-		
+//		char[][] ticTacToeArray2 = {{'O', 'X', 'X'}, {'O', 'X', 'X'}, {'O', 'X', 'X'}}; // declaration
+//		
+//		System.out.println(ticTacToeArray2[2][2]);
+//		
 		//System.out.println(ticTacToeArray2[2][3]); -> Array Index out of bound exception
+		
+		
+		// Print the values of an 2-D array
+		
+//		char row1[] = ticTacToeArray[0];
+//		
+//		for (int i = 0; i < ticTacToeArray.length; i++)
+//		{
+//			System.out.print(row1[i] + " ");
+//		}
+//		
+//		System.out.println();
+//		
+//		char row2[] = ticTacToeArray[1];
+//		
+//		for (int i = 0; i < ticTacToeArray.length; i++)
+//		{
+//			System.out.print(row2[i] + " ");
+//		}
+//		
+//		System.out.println();
+//		
+//		char row3[] = ticTacToeArray[2];
+//		
+//		for (int i = 0; i < ticTacToeArray.length; i++)
+//		{
+//			System.out.print(row3[i] + " ");
+//		}
+		
+		
+//		for (int row=0; row <3; row++)
+//		{
+//			char row2[] = ticTacToeArray[row];
+//			
+//			for (int i = 0; i < ticTacToeArray.length; i++)
+//			{
+//				System.out.print(row2[i] + " ");
+//			}
+//			
+//			System.out.println();
+//		}
+		
+		
+		for (int row=0; row < 3; row++) 
+		{
+			for (int column = 0; column < ticTacToeArray[row].length; column++)
+			{
+				System.out.print(ticTacToeArray[row][column] + " ");
+			}
+			
+			System.out.println();
+		}
+		
+		// row=0 -> 0 < 3 
+			// column = 0, 0 < 3
+				// print [0][0]  -> increment column
+			// column = 1, 1 < 3
+				// print [0][1]  -> increment column
+			// column = 2, 2 < 3
+				// print [0][2]  -> increment column
+			// column = 3, 3 < 3 (false)
+		
+			// new line -> increment row
+		
+		// row=1 -> 1 < 3
+			// column = 0, 0 < 3
+				// print [1][0]  -> increment column
+			// column = 1, 1 < 3
+				// print [1][1]  -> increment column
+			// column = 2, 2 < 3
+				// print [1][2]  -> increment column
+			// column = 3, 3 < 3 (false)
+		
+		// row=1 -> 1 < 3
+			// column = 0, 0 < 3
+				// print [1][0]  -> increment column
+			// column = 1, 1 < 3
+				// print [1][1]  -> increment column
+			// column = 2, 2 < 3
+				// print [1][2]  -> increment column
+			// column = 3, 3 < 3 (false)	
+		
+		
+		// 2-D Array -> Matrix
+		
+		// Matrix Addition, Multiplication, diagonal values, print only last column values
 		
 	}
 
